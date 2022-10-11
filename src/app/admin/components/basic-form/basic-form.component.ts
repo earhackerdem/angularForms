8ef43f2fcda889d9bcbf05c7e18171d80874f770
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-10-11 13:58:12 -0500

    Manejo de binding de inputs radio y checkbox

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -18,6 +18,11 @@ export class BasicFormComponent implements OnInit {
   categoryField = new FormControl('category-2');
   tagField = new FormControl('');
 
+  agreeField = new FormControl(false);
+  genderField = new FormControl('')
+  zoneField = new FormControl('');
+  preferencesField = new FormControl('');
+
 
   constructor() { }
 
